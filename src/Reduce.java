8fc76a84692321972f8199841b480f90da149f37
Bo-Ying Lee <bk.lee@mail.utoronto.ca>
2023-09-15 16:33:55 -0400

    New file - Reduce.java!

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int num = 100;
+        int count = 0;
+        while (num > 0){
+
+
+            // Find out if num is even or odd
+            boolean isEven = num % 2 == 0;
+
+            if (isEven){
+                num = num / 2;
+                count++;
+
+            }
+            else{
+                num--;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
